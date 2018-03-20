1e34235bf8d2235a650c4d42bd637da9a0f0985d
github username <github email address>
2018-03-20 22:27:19 +0000

    Adds styling to the game-detail

--- math-owl/src/containers/game-detail.js
+++ math-owl/src/containers/game-detail.js
@@ -1,9 +1,23 @@
 import React from 'react';
+import { Row, Grid, Form, FormGroup, ControlLabel, FormControl, Col, Button } from 'react-bootstrap';
 
 class GameDetail extends React.Component {
     render() {
         return (
-            <div>2 + 2 =</div>
+            <Form horizontal>
+                <FormGroup controlId="formHorizontalEmail" className="form-group-lg row">
+                    <Col componentClass={ControlLabel} sm={4}>
+                        2 + 2 =
+                    </Col>
+                    <Col sm={2} >
+                        <FormControl type="answer" placeholder="Answer" />
+                    </Col>
+                    <Col sm={2}>
+                        <Button type="submit" className="btn btn-md">Submit</Button>
+                    </Col>
+                </FormGroup>
+            </Form>
+            
         );
     }
 }
