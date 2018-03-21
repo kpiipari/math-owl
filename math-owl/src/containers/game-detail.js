8c6ab84e573747c61eb025d9ee116018dcbcd540
github username <github email address>
2018-03-21 21:19:21 +0000

    Installs react-icons. Adds solid and regular star icons from Font Awesome icon library

--- math-owl/src/containers/game-detail.js
+++ math-owl/src/containers/game-detail.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Row, Grid, Form, FormGroup, ControlLabel, FormControl, Col, Button } from 'react-bootstrap';
+import FaStar from 'react-icons/lib/fa/star';
+import FaStarO from 'react-icons/lib/fa/star-o';
 
 class GameDetail extends React.Component {
     render() {
@@ -9,14 +11,17 @@ class GameDetail extends React.Component {
                     <Col componentClass={ControlLabel} sm={4}>
                         2 + 2 =
                     </Col>
-                    <Col sm={2} >
+                    <Col sm={2}>
                         <FormControl type="answer" placeholder="Answer" />
                     </Col>
                     <Col sm={2}>
                         <Button type="submit" className="btn btn-md">Submit</Button>
                     </Col>
                 </FormGroup>
+                <FaStar size={42}></FaStar>
+                <FaStarO size={42}></FaStarO>
             </Form>
+            
             
         );
     }
