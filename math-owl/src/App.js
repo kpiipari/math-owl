93d76bf1378ee9daf2fca0d1b3c72fcc1e8bd193
github username <github email address>
2018-03-26 23:49:42 +0100

    Updates App.js to only render GameDashboard and Header components

--- math-owl/src/App.js
+++ math-owl/src/App.js
@@ -2,12 +2,15 @@ import React, { Component } from 'react';
 import './App.css';
 
 import GameDashboard from './components/game-dashboard';
+import Header from './components/header';
+
 
 class App extends Component {
+
   render() {
     return (
       <div>
-        
+         <Header />
         <GameDashboard />
       </div>      
     );
