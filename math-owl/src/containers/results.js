bbf898ad51810265d9575b21b0a746b9d9d592bd
github username <github email address>
2018-03-28 23:31:36 +0100

    Updates Results components UI

--- math-owl/src/containers/results.js
+++ math-owl/src/containers/results.js
@@ -1,18 +1,20 @@
 import React from 'react';
-import { Row, Grid, Col, Button } from 'react-bootstrap';
-import EndGame from './end-game';
+import { Grid, Header } from 'semantic-ui-react';
 
 class Results extends React.Component {
     render() {
         return (
             <Grid>
-                <Row>
-                    <Col xs={8} md={6} xsOffset={2}>
-                        <h2>Score: {this.props.score}</h2>
-                        <h2>Time: {this.props.time}</h2>
-                        <Button type="submit" className="btn btn-md">Continue playing...</Button>
-                    </Col>
-                </Row>
+                <Grid.Row centered columns={4}>
+                    <Grid.Column>
+                        <Header as='h2'>
+                            Score: {this.props.score}
+                        </Header>
+                        <Header as='h2'>
+                            Time: {this.props.time}
+                        </Header>
+                    </Grid.Column>
+                </Grid.Row>
             </Grid>
         );
     }
