22cbb3af0e796cd450ce8119dc9309d84ecd8a9a
github username <github email address>
2018-03-28 23:26:44 +0100

    Refactors GameDasboard. GameDashboard determines whether to render GameSelection, GameStart, GameDetail, GameContinue or EndGame components based on states it receives from App.js. It paasses apppropriate props down to sub-components

--- math-owl/src/components/game-dashboard.js
+++ math-owl/src/components/game-dashboard.js
@@ -1,69 +1,80 @@
 import React from 'react';
 
-import SubHeader from './sub-header';
 import GameStart from './game-start';
+import GameContinue from './game-continue';
 import GameSelection from '../containers/game-selection';
 import GameDetail from '../containers/game-detail';
 import Results from '../containers/results';
 import EndGame from '../containers/end-game';
+import RoundCount from '../containers/round-count';
+import FaStar from 'react-icons/lib/fa/star';
+import FaStarO from 'react-icons/lib/fa/star-o';
+
+import { Divider, Grid, Segment } from 'semantic-ui-react';
 
 class GameDashboard extends React.Component {
-    state = {
-        level: 1,
-        game: "Addition",
-        score: 1,
-        time: 0,
-        gameSelected: false,
-        gameStarted: true,
-        gameEnded: false,
-        round1:{var1: 5, var2: 5, answer: 10}, 
-        round2:{var1: 2, var2: 3, answer: 5}
-    };
-
+    
     render() {
-        if (this.state.gameStarted && !this.state.gameEnded) {
+        if (this.props.gameStarted && !this.props.roundEnded) {
             return (
                 <div>
-                    <SubHeader 
-                        game={this.state.game}
-                        level={this.state.level}
-                        gameMode={this.state.gameStarted}
-                    />
-                    <GameDetail score={this.state.score} var1={this.state.round1.var1} var2={this.state.round1.var2}/>
+                    <Grid centered columns={2}>
+                        <Grid.Row verticalAlign='middle'>
+                            <Grid.Column>
+                                <GameDetail score={this.props.score} var1={this.props.round.var1} var2={this.props.round.var2}/>
+                            </Grid.Column>
+                        </Grid.Row>
+                        <Grid.Row verticalAlign='middle'>
+                            <Grid.Column>
+                                <RoundCount score={this.props.score}/>
+                            </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
                 </div>
             );
-        } else if (this.state.gameStarted) {
-            return (
-            <div>
-                <SubHeader 
-                        game={this.state.game}
-                        level={this.state.level}
-                        gameMode={this.state.gameStarted}
-                />
-                <Results score="8" time="10 sec" />
-                <EndGame />
-            </div>
-            ); 
-        } else if (!this.state.gameSelected){
+        } else if (this.props.gameStarted && this.props.roundEnded && !this.props.gameEnded) {
             return (
                 <div>
-                    <SubHeader 
-                        game={this.state.game}
-                        level={this.state.level}
-                        gameMode={this.state.gameSelected}
-                    />
-                    <GameSelection />
+                    <Grid centered columns={2}>
+                        <Grid.Row verticalAlign='middle'>
+                            <Grid.Column>
+                                <Results score={this.props.score} time={this.props.time}/>
+                                <GameContinue level={this.props.level} time={this.props.time}/>
+                            </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
+                </div>
+            ); 
+        } else if (!this.props.gameSelected){
+            return (
+                <div>
+                    <Grid centered columns={2}>
+                        <Grid.Row verticalAlign='middle'>
+                            <GameSelection />
+                        </Grid.Row>
+                    </Grid>
+                </div>
+            )
+        } else if (this.props.gameEnded){
+            console.log(this.props.gameEnded)
+            return (
+                <div>
+                    <Grid centered columns={2}>
+                        <Grid.Row verticalAlign='middle'>
+                            <EndGame score={this.props.score}/>
+                        </Grid.Row>
+                    </Grid>
                 </div>
             )
         } else {
             return (
                 <div>
-                    <SubHeader 
-                        game={this.state.game}
-                        level={this.state.level}
-                        gameMode={this.state.gameSelected}
-                    />
-                    <GameStart />
+                    <Grid centered columns={2}>
+                        <Grid.Row verticalAlign='middle'>
+                            <Divider hidden />
+                            <GameStart />
+                        </Grid.Row>
+                    </Grid>
                 </div>
             )
         }
