165a6d434aa2df0a47848559421c5bac7f98962d
github username <github email address>
2018-03-29 21:04:58 +0100

    Updates RoundCount component to use Grid from semantic ui

--- math-owl/src/containers/round-count.js
+++ math-owl/src/containers/round-count.js
@@ -1,18 +1,18 @@
 import React from 'react';
-import { Row, Grid, Col } from 'react-bootstrap';
+import { Divider, Grid, Segment } from 'semantic-ui-react';
 import FaStar from 'react-icons/lib/fa/star';
 import FaStarO from 'react-icons/lib/fa/star-o';
 
 class RoundCount extends React.Component {
     render() {
         return (
-            <Grid>
-                <Row>
-                    <Col xs={8} md={6} xsOffset={2}>
-                        {Array.from({ length: this.props.score}, (v, k) => <FaStar key={k} size={42}></FaStar>)}
-                        {Array.from({ length: 10 - this.props.score}, (v, k) => <FaStarO key={k} size={42}></FaStarO>)}
-                    </Col>
-                </Row>
+            <Grid centered>
+                <Grid.Row verticalAlign='middle'>
+                    <Grid.Column>
+                    {Array.from({ length: this.props.score}, (v, k) => <FaStar key={k} size={42}></FaStar>)}
+                    {Array.from({ length: 10 - this.props.score}, (v, k) => <FaStarO key={k} size={42}></FaStarO>)}
+                    </Grid.Column>
+                </Grid.Row>
             </Grid>
         )
     }
