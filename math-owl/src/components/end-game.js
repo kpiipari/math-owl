bbd99911500c505de08fe6c24c4efaa70d4a3ef3
github username <github email address>
2018-04-03 22:20:25 +0100

    Fixes EndGame UI to make the component centered

--- math-owl/src/components/end-game.js
+++ math-owl/src/components/end-game.js
@@ -1,20 +1,18 @@
 import React from 'react';
-import { Form, Input, Button, Container } from 'semantic-ui-react';
+import { Form, Input, Button } from 'semantic-ui-react';
 
 class EndGame extends React.Component {
     render() {
         return (
-            <Container>
-                    <Form size={'large'}>
-                        <Form.Group>
-                            <Form.Field inline>
-                                <label>Your score: {this.props.score}</label>
-                                <Input placeholder='Your name' />
-                            </Form.Field>
-                            <Button type='submit'>Submit Score</Button>
-                        </Form.Group>
-                    </Form>
-            </Container>
+            <Form size={'large'}>
+                <Form.Group>
+                    <Form.Field inline>
+                        <label>Your score: {this.props.score}</label>
+                        <Input placeholder='Your name' onChange={this.props.handleNameChange}/>
+                    </Form.Field>
+                    <Button type='submit'>Submit Score</Button>
+                </Form.Group>
+            </Form>
         );
     }
 }
