10740428db73965222db74ed9b625b60e0e36ef1
github username <github email address>
2018-04-04 22:54:23 +0100

    Improves GameStart UI

--- math-owl/src/components/game-start.js
+++ math-owl/src/components/game-start.js
@@ -1,14 +1,25 @@
 import React from 'react';
-import { Icon, Header } from 'semantic-ui-react';
+import { Icon, Header, Segment } from 'semantic-ui-react';
 
 class GameStart extends React.Component {
     render() {
         return (
             <div>
-                <Header as='h2' icon>
-                    <Icon name='play' onClick={this.props.handleGameStart}/>
+                <Segment textAlign='center' style={{ minHeight: 100, padding: '6em 6em' }} vertical>
+                    <Header 
+                        as='h1'
+                        textAlign='center'
+                        inverted
+                        style={{
+                        fontSize: '4em',
+                        fontWeight: 'normal',
+                        marginBottom: 0,
+                        marginTop: '0em',
+                        }}>
+                        <Icon name='play' onClick={this.props.handleGameStart}/>
                         Start
-                </Header>
+                    </Header>
+                </Segment>
             </div>
         );
     }
