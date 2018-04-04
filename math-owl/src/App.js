ae2d9d8d28fb74b7f28bb3cc20cdc3c201445d94
github username <github email address>
2018-04-04 08:30:08 +0100

    Adds React router and home and games routes

--- math-owl/src/App.js
+++ math-owl/src/App.js
@@ -2,17 +2,22 @@ import React, { Component } from 'react';
 import './App.css';
 
 import Header from './components/header';
+import Home from './components/home';
 import GameContainer from './containers/game-container';
-
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 class App extends Component {
 
   render() {
       return (
-          <div>
-              <Header />
-              <GameContainer />
-          </div>
+            <Router>
+                <div>
+                    <Header />
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/games" component={GameContainer}/>
+                </div>
+            </Router>
+
       )
     }
 }
