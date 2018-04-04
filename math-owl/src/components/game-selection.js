83a97a954d05257891e738941037056601354936
github username <github email address>
2018-04-04 22:49:19 +0100

    Improves GameSelection UI

--- math-owl/src/components/game-selection.js
+++ math-owl/src/components/game-selection.js
@@ -5,28 +5,30 @@ class GameSelection extends React.Component {
     render() {
         return (
             <div>
-                <Grid>
-                    <Grid.Row columns={3}>
-                        <Grid.Column>
-                            <Header as='h2' icon>
-                                <Icon name='plus' id="Add" size='massive' onClick={this.props.handleGameSelection}/>
-                                Add
-                            </Header>
-                        </Grid.Column>
-                        <Grid.Column>
-                            <Header as='h2' icon>
-                                <Icon name='minus' id="Substract" size='massive' onClick={this.props.handleGameSelection}/>
-                                Substract
-                            </Header>
-                        </Grid.Column>
-                        <Grid.Column>
-                            <Header as='h2' icon>
-                                <Icon name='asterisk' id="Multiply" size='massive' onClick={this.props.handleGameSelection}/> 
-                                Multiply
-                            </Header>
-                        </Grid.Column>
-                    </Grid.Row>
-                </Grid>
+                <Segment textAlign='center' style={{ minHeight: 300, padding: '6em 8em' }} vertical>
+                    <Grid>
+                        <Grid.Row columns={3}>
+                            <Grid.Column>
+                                <Header as='h1' icon>
+                                    <Icon name='plus' id="Add" size='massive' onClick={this.props.handleGameSelection}/>
+                                    Add
+                                </Header>
+                            </Grid.Column>
+                            <Grid.Column>
+                                <Header as='h1' icon>
+                                    <Icon name='minus' id="Substract" size='massive' onClick={this.props.handleGameSelection}/>
+                                    Substract
+                                </Header>
+                            </Grid.Column>
+                            <Grid.Column>
+                                <Header as='h1' icon>
+                                    <Icon name='asterisk' id="Multiply" size='massive' onClick={this.props.handleGameSelection}/> 
+                                    Multiply
+                                </Header>
+                            </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
+                </Segment>
             </div>
         );
     }
