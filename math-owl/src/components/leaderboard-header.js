39cccd17c83e0f0c3a5127695c4f8cee41d18ce3
github username <github email address>
2018-04-11 08:20:07 +0100

    Aligns Leaderboard header title

--- math-owl/src/components/leaderboard-header.js
+++ math-owl/src/components/leaderboard-header.js
@@ -8,10 +8,11 @@ class LeaderboardHeader extends React.Component {
     render() {
         return (
             <Segment textAlign='center' inverted style={{ minHeight: 100, padding: '1em 1em' }} vertical>
-                    <Grid centered columns={4}>
+                    <Grid centered columns={1}>
                         <Grid.Column>
                             <Header
                                 as='h1'
+                                textAlign='center'
                                 inverted
                                 style={{
                                 fontSize: '4em',
