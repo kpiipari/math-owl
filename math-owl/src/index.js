4e4ada522972b9d8dc57d52d0e64c82358ebf38c
github username <github email address>
2018-05-03 22:27:26 +0100

    Adds provider and store to index.js

--- math-owl/src/index.js
+++ math-owl/src/index.js
@@ -1,15 +1,22 @@
 import 'semantic-ui-css/semantic.min.css';
 import './semantic/dist/semantic.min.css';
 import './index.css';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
-
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
 import App from './App';
 
+const store = configureStore();
+
+
 ReactDOM.render((
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>
 ), document.getElementById('root'));
 
