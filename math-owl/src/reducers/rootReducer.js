b07c4fc20f721c4aa0d9aa0c9eee99ab62c57446
github username <github email address>
2018-05-03 22:10:13 +0100

    Adds rootReducer that combines all reducers into one rootReducer

---
+++ math-owl/src/reducers/rootReducer.js
@@ -0,0 +1,8 @@
+import { combineReducers } from 'redux';
+import additionGame from './additionReducer';
+
+const rootReducer = combineReducers({
+    additionGame
+});
+
+export default rootReducer;
