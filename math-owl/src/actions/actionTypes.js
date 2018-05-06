470033a8551c057396c14608400bf51b42c182f1
github username <github email address>
2018-05-06 20:08:43 +0100

    Adds action types from state properties

--- math-owl/src/actions/actionTypes.js
+++ math-owl/src/actions/actionTypes.js
@@ -1,4 +1,16 @@
 export const FETCH_GAME = 'FETCH_GAME';
 export const RECEIVE_GAME = 'RECEIVE_GAME';
+export const LEVEL = 'LEVEL';
+export const GAME = 'GAME';
+export const SCORE = 'SCORE';
+export const TIME = 'TIME';
+export const PLAYER = 'PLAYER';
+export const GAME_SELECTED = 'GAME_SELECTED';
+export const GAME_STARTED = 'GAME_STARTED';
+export const ROUND_ENDED = 'ROUND_ENDED';
+export const GAME_ENDED = 'GAME_ENDED';
+export const ROUND_FETCHED_SUCCESS = 'ROUND_FETCHED_SUCCESS';
+export const ROUND_FETCHED_FAIL = 'ROUND_FETCHED_FAIL';
+export const ANSWER = 'ANSWER';
 
 export const API_URL = process.env.REACT_APP_API_URL;
