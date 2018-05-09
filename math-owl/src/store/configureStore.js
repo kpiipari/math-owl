e629da4cc15632b05a8f0cc20a31844480eb142f
github username <github email address>
2018-05-09 19:18:43 +0100

    Removes initialState from configureStore

--- math-owl/src/store/configureStore.js
+++ math-owl/src/store/configureStore.js
@@ -2,10 +2,9 @@ import { createStore, applyMiddleware } from 'redux';
 import rootReducer from '../reducers/rootReducer';
 import thunk from 'redux-thunk';
 
-export default function configureStore(initialState) {
+export default function configureStore() {
     return createStore(
         rootReducer,
-        initialState,
         window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
         applyMiddleware(thunk)
     );
