5971f567851c66706f262f20710c6bc71330b924
github username <github email address>
2018-05-21 14:24:49 +0100

    Fixes an issue with the Leaderboard header alignment

--- math-owl/src/components/leaderboard.js
+++ math-owl/src/components/leaderboard.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Header, Table, Segment, Grid } from 'semantic-ui-react'
+import { Table, Segment, Grid } from 'semantic-ui-react'
 
 
 class Leaderboard extends React.Component {
@@ -10,14 +10,9 @@ class Leaderboard extends React.Component {
             return <p>Sorry! Unable to load the leaderboard data</p>;  
         } if (this.props.loading) {
             return (
-                <div>
-                    <Table.Row>
+                <Table.Row>
                     <Table.Cell>
-                    <Header as='h4' inverted>
-                        <Header.Content>
-                            loading...
-                        </Header.Content>
-                    </Header>
+                        loading...
                     </Table.Cell>
                     <Table.Cell>
                         loading...
@@ -25,52 +20,47 @@ class Leaderboard extends React.Component {
                     <Table.Cell>
                         loading...
                     </Table.Cell>
-                    </Table.Row>
-                </div>
+                </Table.Row>
             )
-        } else {
-        const data = this.props.players;
-        console.log(data)
-        return (
-            data.map((player, index) =>
-                <div key={index + 1}>
+          } else {
+            const data = this.props.players;
+            console.log(data)
+            return (
+                data.map((player, index) =>
                     <Table.Row>
-                    <Table.Cell>
-                     {index + 1}
-                    </Table.Cell>
-                    <Table.Cell>
-                        {player.name}
-                    </Table.Cell>
-                    <Table.Cell>
-                        {player.total_score}
-                    </Table.Cell>
+                        <Table.Cell>
+                        {index + 1}
+                        </Table.Cell>
+                        <Table.Cell>
+                            {player.name}
+                        </Table.Cell>
+                        <Table.Cell>
+                            {player.total_score}
+                        </Table.Cell>
                     </Table.Row>
-                </div>
-            ))
-        }
+                ))
+            }
     } 
 
     render() { 
     return (
         <Segment textAlign='center' style={{ minHeight: 300, padding: '6em 8em' }} vertical>
             <Grid centered columns={2}>
-                <Grid.Row verticalAlign='middle'>
-                    <Table size='large' inverted celled collapsing>
+               
+                    <Table inverted>
                         <Table.Header>
-                        <Table.Row>
-                            <Table.HeaderCell>Rank</Table.HeaderCell>
-                            <Table.HeaderCell>Name</Table.HeaderCell>
-                            <Table.HeaderCell>Score</Table.HeaderCell>
-                            <Table.HeaderCell>Time</Table.HeaderCell>
-                        </Table.Row>
-                        </Table.Header>
-                        <Table.Body>
                             <Table.Row>
-                                {this.generateLeaderboard()}
+                                <Table.HeaderCell>Rank</Table.HeaderCell>
+                                <Table.HeaderCell>Name</Table.HeaderCell>
+                                <Table.HeaderCell>Score</Table.HeaderCell>
                             </Table.Row>
+                        </Table.Header>
+
+                        <Table.Body>
+                            {this.generateLeaderboard()}
                         </Table.Body>
                     </Table>
-                </Grid.Row>
+              
             </Grid>
         </Segment>
     )}    
